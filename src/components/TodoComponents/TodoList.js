544a3a141dd93e277cee006394ecd00a88bf335a
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-24 12:30:01 -0700

    Added a few to-do items in an array

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,24 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from "react";
+import Todo from "./Todo"; 
+
+const TodoList = [
+  {
+    task: 'Build to-do app',
+    id: 0,
+    completed: false
+  }, 
+
+  {
+    task: 'Review training kit',
+    id: 1,
+    completed: false
+  },
+  {
+    task: 'Make Dolly Parton succulent holders',
+    id: 2,
+    completed: false
+  },
+];
