f7edf34d77770311320fa7128a4b378711d5e588
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-24 12:57:47 -0700

    Mounted static array of todos to the DOM

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,12 @@
+// Child of Parent TodoList
+
+import React from 'react'; 
+
+function Todo(props) {
+    return (
+        <div>
+            <p>{props.todo.task}</p>
+        </div>
+    )
+}
+export default Todo; 
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,10 +1,9 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
-
+// Establishing my imports 
 import React from "react";
 import Todo from "./Todo"; 
 
-const TodoList = [
+// Building my array of to-do's 
+const todos = [
   {
     task: 'Build to-do app',
     id: 0,
@@ -22,3 +21,45 @@ const TodoList = [
     completed: false
   },
 ];
+
+// Creating my Class component
+
+// 1) Declaring the Class 
+
+class TodoList extends React.Component {
+    // 2) Adding my constructor function 
+    constructor () {
+        super(); 
+        // Will add more to state 
+        this.state = {
+            todosOnState: todos,
+            todo: {
+                task: '',
+                id: '',
+                completed: false
+            }
+        };
+    }
+
+
+// Space for additional functions 
+
+// 3) Render UI and return some JSX 
+render() {
+    return (
+        <div>
+            <h1>Kimberlee's to-do's</h1>
+
+            <div>
+                {this.state.todosOnState.map(todo =>
+                (
+                  <Todo todo={todo} />
+                )  
+                )}
+            </div>
+        </div>
+    );
+}
+}
+
+export default TodoList; 
